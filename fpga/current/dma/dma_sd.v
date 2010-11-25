66a6c4129684d81253a24abd7c8ef2898b283dc8
lvd <lvd@65a45dc7-6f19-d546-965d-19a258bef92e>
2010-11-25 08:06:32 +0000

    fpga/current: split dma_sd and dma_fifo_oneshot

--- fpga/current/dma/dma_sd.v
+++ fpga/current/dma/dma_sd.v
@@ -150,21 +150,23 @@ module dma_sd(
 	// connect dma in to fifo out without muxing
 	assign dma_wd = fifo_rd;
 
-	fifo512_oneshot fifo512_oneshot ( .clk(clk),
-	                                  .rst_n(rst_n),
+	dma_fifo_oneshot sd_fifo(
+		.clk(clk),
+		.rst_n(rst_n),
 
-	                                  .init(init),
+		.init(init),
 
-	                                  .wr_stb(wr_stb),
-	                                  .rd_stb(rd_stb),
+		.wr_stb(wr_stb),
+		.rd_stb(rd_stb),
 
-	                                  .wdone(wdone),
-	                                  .rdone(rdone),
-	                                  .empty(empty),
+		.wdone(wdone),
+		.rdone(rdone),
+		.empty(empty),
 
-	                                  .wd(fifo_wd),
-	                                  .rd(fifo_rd)
-	                                );
+		.wd(fifo_wd),
+		.rd(fifo_rd)
+	);
+
 	// fifo control
 	reg sd_wr_stb,sd_rd_stb;   // set in SD FSM
 	reg dma_wr_stb,dma_rd_stb; // set in DMA FSM
@@ -466,108 +468,8 @@ module dma_sd(
 		end
 	end
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 endmodule
 
 
 
 
-// this is "one-shot" fifo: after each 512 bytes both written and read back, it must be initialized by means of 'init'
-//
-module fifo512_oneshot(
-
-	input  wire clk,
-	input  wire rst_n,
-
-	input  wire init, // initializes fifo: wptr=rptr=0
-
-	input  wire wr_stb, // write strobe: writes data from wd to the current wptr, increments wptr
-	input  wire rd_stb, // read strobe: increments rptr
-
-	output wire wdone, // write done - all 512 bytes are written (end of write operation)
-	output wire rdone, // read done - all 512 bytes are read (end of read operation)
-	output wire empty, // fifo empty: when wptr==rptr (rd_stb must not be issued when empty is active, otherwise everytrhing desyncs)
-
-	input  wire [7:0] wd, // data to be written
-	output wire [7:0] rd  // data just read from rptr address
-);
-
-	reg [9:0] wptr;
-	reg [9:0] rptr;
-
-	always @(posedge clk, negedge rst_n)
-	begin
-		if( !rst_n )
-		begin
-			wptr = 10'd0;
-			rptr = 10'd0;
-		end
-		else
-		begin // posedge clk
-
-			if( init )
-			begin
-				wptr <= 10'd0;
-			end
-			else if( wr_stb )
-			begin
-				wptr <= wptr + 10'd1;
-			end
-
-
-			if( init )
-			begin
-				rptr <= 10'd0;
-			end
-			else if( rd_stb )
-			begin
-				rptr <= rptr + 10'd1;
-			end
-
-		end
-	end
-
-	assign wdone = wptr[9];
-	assign rdone = rptr[9];
-	assign empty = ( wptr==rptr );
-
-
-
-	mem512b fifo512_oneshot_mem512b( .clk(clk),
-
-	                                 .rdaddr(rptr[8:0]),
-	                                 .dataout(rd),
-
-	                                 .wraddr(wptr[8:0]),
-	                                 .datain(wd),
-	                                 .we(wr_stb)
-	                               );
-endmodule
