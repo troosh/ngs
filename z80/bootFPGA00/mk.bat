f3e139db25065efe7cd852a267235bd57c4eea68
lvd <lvd@65a45dc7-6f19-d546-965d-19a258bef92e>
2011-01-16 23:51:54 +0000

    fpga/current: fresh build, z80/bootFPGA: fresh build (NOT CHECKED YET!)

--- z80/bootFPGA00/mk.bat
+++ z80/bootFPGA00/mk.bat
@@ -1,4 +1,4 @@
-asw -cpu z80undoc -L %1.asm
-p2hex -r $-$ -F Intel %1.p
-p2bin -r $-$ -l 0xFF %1.p
+..\..\tools\asl\asw -cpu z80undoc -L %1.asm
+..\..\tools\asl\p2hex -r $-$ -F Intel %1.p
+..\..\tools\asl\p2bin -r $-$ -l 0xFF %1.p
 
