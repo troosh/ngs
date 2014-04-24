3d08c93df1bed8da3eab3f8ef55b92e29ae6193f
savelij <savelij@65a45dc7-6f19-d546-965d-19a258bef92e>
2014-04-24 17:58:25 +0000

    Еще немного переделал сборщики.

--- z80/create_update/create_full_rom.bat
+++ z80/create_update/create_full_rom.bat
@@ -5,10 +5,14 @@ cd ..\main_rom
 ..\..\tools\asw\asw -U -L main_ngs.a80
 ..\..\tools\asw\p2bin main_ngs.p neogs.rom -r $-$ -k
 
+del *.lst
+
 cd ..\loader_ngs
 
 ..\..\tools\asw\asw -U -L loader_ngs.a80
 ..\..\tools\asw\p2bin loader_ngs.p loader_ngs.rom -r $-$ -k
+
+del *.lst
 
 cd ..\bootFPGA00
 
@@ -22,7 +26,6 @@ call mk.bat bootFPGA
 
 del *.lst
 del *.mlz
-del *.bin
 del *.rbf
 del *.hex
 
@@ -30,21 +33,13 @@ cd ..\create_update
 
 copy /B /Y ..\loader_ngs\loader_ngs.rom
 copy /B /Y ..\main_rom\neogs.rom
-addcrc loader_ngs.rom
-ren crc.bin loader.crc
-addcrc fpga.bin
-ren crc.bin fpga.crc
-addcrc neogs.rom
-ren crc.bin neogs.crc
+copy /B /Y ..\bootFPGA00\bootFPGA.crc fpga.bin
 
 ..\..\tools\asw\asw -U -L make_full_rom.a80
 ..\..\tools\asw\p2bin make_full_rom.p full_ngs.rom -r $-$ -k
 
 del loader_ngs.rom
 del neogs.rom
-del loader.crc
-del fpga.crc
-del neogs.crc
 del *.lst
 
 copy /B /Y full_ngs.rom D:\UnrealSpeccy\bootgs.bin
