4886b37cefa6a2ebcb44d6c5a582ded0a2dc84fe
savelij <savelij@65a45dc7-6f19-d546-965d-19a258bef92e>
2014-08-08 15:41:03 +0000

    Прошивка NeoGS 1.10 и Neo player light 0.52

--- z80/main_rom/make_rom.bat
+++ z80/main_rom/make_rom.bat
@@ -1,11 +1,11 @@
 @ECHO OFF
 
 ..\..\tools\asw\asw -U -L main_ngs.a80
-..\..\tools\asw\p2bin main_ngs.p NEOGS.ROM -r $-$ -k
+..\..\tools\asw\p2bin main_ngs.p neogs.rom -r $-$ -k
 
 ..\..\tools\asw\asw -U -L main_full.a80
 ..\..\tools\asw\p2bin main_full.p full_ngs.rom -r $-$ -k
 
-del *.lst
+rem del *.lst
 
 pause
